3e4eecd04fee110a4aff09dc3f504e9e94744668
hassey13 <ehasselbring@gmail.com>
2017-05-04 17:56:39 -0400

    test server for sockets issue

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
+
+io.on('connection', function(socket){
+  console.log('User connected!')
+
+  socket.on('move', function( board ){
+    io.emit('move', board )
+  })
+
+  socket.on('invite', function( invite ){
+    io.emit('invite', invite )
+  })
+
+  socket.on('acceptedInvite', function( invite ){
+    io.emit('acceptedInvite', invite )
+  })
+})
+
+const port = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || 'production';
+
+server.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Server running on http://localhost:${port} [${env}]`);
+})
